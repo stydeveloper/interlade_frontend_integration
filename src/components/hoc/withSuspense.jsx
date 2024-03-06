f8add13d305f46bd2c6c2480b6d10ede63680903
stydeveloper <stallyons.tester2@gmail.com>
2024-03-06 20:56:32 +0500

    a

--- src/components/hoc/withSuspense.jsx
+++ src/components/hoc/withSuspense.jsx
@@ -3,7 +3,7 @@ import { Suspense } from "react";
 function withSuspense(Component) {
   return function WithSuspense(props) {
     return (
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div />}>
         <Component {...props} />
       </Suspense>
     );
