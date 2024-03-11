7975c80dd5d3d82c2f91408baf5a296faf75216c
stydeveloper <stallyons.tester2@gmail.com>
2024-03-11 11:39:00 +0500

    a

--- src/app/createbol/page.jsx
+++ src/app/createbol/page.jsx
@@ -384,7 +384,7 @@ export default function Page() {
 
         allNotificationsRefetchFunction();
         unreadCountRefetchFunction();
-        // recentBolsRefetchFunction();
+        recentBolsRefetchFunction();
 
         router.push("/");
       }
