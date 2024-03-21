d3614631c97b0a44c5ed6d77064fd83578b44938
stydeveloper <stallyons.tester2@gmail.com>
2024-03-21 16:58:17 +0500

    a

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -27,7 +27,6 @@ export default function Home() {
     } else if (hasAcknowledgedTerms === "false") {
       setTermsOpen(false);
     }
-    setLoading(false);
   }, []); // Empty dependency array ensures that the effect runs only once
 
   const handleTermsAcknowledgement = () => {
